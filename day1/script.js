36e771aafe9a9c3feb4022ff5734ce29489fbaf6
srinivasansureshmech <srinivasansureshmech@gmail.com>
2023-08-22 20:57:22 +0530

    rough

--- day1/script.js
+++ day1/script.js
@@ -1,1 +1,25 @@
-console.log("Passed")
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+
+ let N = parseInt(userInput[0]);
+ for(i=1;i<=N;i++){
+    console.log(i*9);
+ }
+
+
+//end-here
+});
---
+++ day2/script.js
@@ -0,0 +1,11 @@
+let x=5;
+let y= x;
+x=10
+console.log(x);
+console.log(y);
+
+let arr1=[1,2,3]
+let arr2=arr1;
+arr1.push(4)
+console.log(arr1)
+console.log(arr2)
