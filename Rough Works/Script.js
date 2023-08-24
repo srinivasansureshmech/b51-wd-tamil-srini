92eb6bbeb14ac25355800cda5273d9d2bdae26b0
srinivasansureshmech <srinivasansureshmech@gmail.com>
2023-08-24 14:47:01 +0530

    Task4

---
+++ Rough Works/Script.js
@@ -0,0 +1,6 @@
+
+var greet = function (platform) {
+    console.log("Welcome to ",platform);
+};
+  
+greet('GeeksforGeeks!');
---
+++ Tasks/Day 4 task/Anonymous Function/Print odd numbers in an array.js
@@ -0,0 +1,9 @@
+// Print odd numbers in an array
+let Arr=[1,2,3,4,5];
+    const odds = Arr.filter(function(num){return num % 2 === 1});
+    console.log(odds);
+
+
+//Convert all the strings to title caps in a string array
+let strArr=['srini','dhana','siva','padma'];
+let toUpper=toUpperCase(strArr[0])
