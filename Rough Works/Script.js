5706a68d604bf5ec0ee2f7cbb9f8aae2896d30c2
srinivasansureshmech <srinivasansureshmech@gmail.com>
2023-08-29 14:52:12 +0530

    rough

--- Rough Works/Script.js
+++ Rough Works/Script.js
@@ -1,6 +1,27 @@
 
-var greet = function (platform) {
-    console.log("Welcome to ",platform);
-};
+// var greet = function (platform) {
+//     console.log("Welcome to ",platform);
+// };
   
-greet('GeeksforGeeks!');
+// greet('GeeksforGeeks!');
+
+// function proto(name){
+//     this.name=name
+// }
+// proto1=new proto('Srini');
+// console.log(proto1);
+
+function Student(name) {
+        this.name = name;
+    
+        this.greet = function () {
+            console.log(`Hello! my name is ${this.name}`);
+        }
+    }
+    let student101 = new Student('Krish');
+let student102 = new Student('Sathish');
+
+let students = [student101, student102];
+student102.greet();
+// student101('Srini')
+console.log(student101);
