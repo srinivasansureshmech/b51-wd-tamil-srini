cb1cba5a401c0790948c0a7eab8eb70f0a3c1f73
srinivasansureshmech <srinivasansureshmech@gmail.com>
2023-08-30 16:25:10 +0530

    added xhr2

--- Rough Works/Script.js
+++ Rough Works/Script.js
@@ -11,17 +11,26 @@
 // proto1=new proto('Srini');
 // console.log(proto1);
 
-function Student(name) {
-        this.name = name;
+// function Student(name) {
+//         this.name = name;
     
-        this.greet = function () {
-            console.log(`Hello! my name is ${this.name}`);
-        }
-    }
-    let student101 = new Student('Krish');
-let student102 = new Student('Sathish');
+//         this.greet = function () {
+//             console.log(`Hello! my name is ${this.name}`);
+//         }
+//     }
+//     let student101 = new Student('Krish');
+// let student102 = new Student('Sathish');
 
-let students = [student101, student102];
-student102.greet();
-// student101('Srini')
-console.log(student101);
+// let students = [student101, student102];
+// student102.greet();
+// // student101('Srini')
+// console.log(student101);
+
+// function isBigEnough(value) {
+//     return value >= 10;
+//   }
+  
+  const filtered = [12, 5, 8, 130, 44]
+  // filtered is [12, 130, 44]
+  console.log(  filtered.filter((value)=>value>=10));
+  
---
+++ Tasks/Day 7 task/Asia.js
@@ -0,0 +1,27 @@
+//To print all countries flags
+
+let XMLHttpRequest = require('xhr2');
+
+let xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'https://restcountries.com/v3.1/all');
+
+xhr.onload = function () {
+    
+    let users = JSON.parse(xhr.responseText);
+
+    // let userNames = [];
+
+    // for (let user of users) {
+    //     userNames.push(user['flag']);
+    // }
+
+    // console.log(userNames);
+    let Asian = users.filter((continents)=> {
+        if(continents=="Asia"){
+            return continents;
+        }});
+    console.log(Asian);
+};
+
+xhr.send();
